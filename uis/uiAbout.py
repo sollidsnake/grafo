81405e569dd1373adfc4b6baa861d227cb25397d
Jesse Nazario <jessenzr@gmail.com>
2014-11-15 17:50:19 -0200

    commit uis

---
+++ uis/uiAbout.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'uis/about.ui'
+#
+# Created: Thu Nov 13 19:06:07 2014
+#      by: PyQt4 UI code generator 4.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName(_fromUtf8("Form"))
+        Form.resize(403, 341)
+        self.gridLayout = QtGui.QGridLayout(Form)
+        self.gridLayout.setSizeConstraint(QtGui.QLayout.SetFixedSize)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.tbAbout = QtGui.QTextBrowser(Form)
+        self.tbAbout.setMinimumSize(QtCore.QSize(0, 291))
+        self.tbAbout.setObjectName(_fromUtf8("tbAbout"))
+        self.gridLayout.addWidget(self.tbAbout, 0, 0, 1, 1)
+        self.pushFechar = QtGui.QPushButton(Form)
+        self.pushFechar.setObjectName(_fromUtf8("pushFechar"))
+        self.gridLayout.addWidget(self.pushFechar, 1, 0, 1, 1)
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(_translate("Form", "Sobre", None))
+        self.pushFechar.setText(_translate("Form", "&Fechar", None))
+
---
+++ uis/uiResultado.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'uis/resultado.ui'
+#
+# Created: Thu Nov 13 19:49:42 2014
+#      by: PyQt4 UI code generator 4.11.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Resultado(object):
+    def setupUi(self, Resultado):
+        Resultado.setObjectName(_fromUtf8("Resultado"))
+        Resultado.resize(340, 381)
+        self.gridLayout = QtGui.QGridLayout(Resultado)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.tbResultado = QtGui.QTextBrowser(Resultado)
+        self.tbResultado.setObjectName(_fromUtf8("tbResultado"))
+        self.gridLayout.addWidget(self.tbResultado, 0, 0, 1, 1)
+        self.pushFechar = QtGui.QPushButton(Resultado)
+        self.pushFechar.setObjectName(_fromUtf8("pushFechar"))
+        self.gridLayout.addWidget(self.pushFechar, 1, 0, 1, 1)
+
+        self.retranslateUi(Resultado)
+        QtCore.QMetaObject.connectSlotsByName(Resultado)
+
+    def retranslateUi(self, Resultado):
+        Resultado.setWindowTitle(_translate("Resultado", "Resultado", None))
+        self.pushFechar.setText(_translate("Resultado", "&Fechar", None))
+
